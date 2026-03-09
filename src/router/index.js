3077ed0897ec58376ec598895bd68158704725bc
Faith <fwkimanii@gmail.com>
2026-03-09 09:42:44 +0300

    fix router import for PropertyDetails

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import propertylist from '../views/propertylist.vue'
-import propertyDetails from '../views/propertyDetails.vue'
+import PropertyDetails from '../views/PropertyDetails.vue'
 
 const routes = [
   { path: '/', name: 'propertylist', component: propertylist },
-  { path: '/property/:id', name: 'propertyDetails', component: propertyDetails, props: true }
+  { path: '/property/:id', name: 'PropertyDetails', component: PropertyDetails, props: true }
 ]
 
 const router = createRouter({
